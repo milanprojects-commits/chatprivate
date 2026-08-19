a7a158547eda226dd31629521c936e2ea976edd4
milanprojects-commits <pruijsmilan10@gmail.com>
2026-08-19 14:25:47 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -302,4 +302,8 @@ def get_signals():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000)),
+        debug=False
+    )
